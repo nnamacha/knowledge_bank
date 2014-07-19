93575fe9a52584e4b50b50bc1257030fe40541f4
nnamacha <nnamacha@gmail.com>
2014-07-19 18:51:02 +0000

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KnowledgeBank::Application.config.secret_key_base = '18fc8dbbf1285e17f97a3421a7c11752fe11ef9b6b10892fdde5ac77699b55e813f36d3c2866d6613be3e86783291f13a03f0222a0786c1b7f88b9af42cead30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KnowledgeBank::Application.config.session_store :cookie_store, key: '_knowledge_bank_session'
